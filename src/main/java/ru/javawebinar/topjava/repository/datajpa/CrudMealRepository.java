f6fff6bfbd13aaa911137ee8550f14ab9a959775
pavell00 <mymmo88@gmail.com>
2017-01-20 10:40:50 +0300

    5_6

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -35,7 +35,7 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     List<Meal> findAll(@Param("userId") int userId);
 
     @Query("SELECT m FROM Meal m " +
-            "WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
+            "WHERE m.user.id=:userId AND m.dateTime >=:startDate AND m.dateTime<=:endDate ORDER BY m.dateTime DESC")
     List<Meal> getBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate,
                           @Param("userId") int userId);
 
